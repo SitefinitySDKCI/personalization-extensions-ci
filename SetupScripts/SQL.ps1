11565c5c0ad7ab69c0adaff86f48ca6dae48856f
nader-dab <nader.dabour@abv.bg>
2014-06-02 18:47:43 +0300

    Modified SQL.ps1

--- SetupScripts/SQL.ps1
+++ SetupScripts/SQL.ps1
@@ -55,7 +55,7 @@ function AttachDatabase($databaseServer, $dbName, $restoredDbName, $dbLocation)
     "debug ------> " + $dbDestinationFullPath
     "debug ------> " + $logDestinationFullPath
     
-    $sqlCmdExe -S $databaseServer -E -R -q "exit(CREATE DATABASE [$dbName] ON ( FILENAME = N'$dbDestinationFullPath' ), ( FILENAME = N'$logDestinationFullPath' ) FOR ATTACH)"
+    SQLCMD.EXE -S $databaseServer -E -R -q "exit(CREATE DATABASE [$dbName] ON ( FILENAME = N'$dbDestinationFullPath' ), ( FILENAME = N'$logDestinationFullPath' ) FOR ATTACH)"
     
     #$mdfFileName = $dbName+".mdf"
     #$ldfFileName = $dbName+"_log.ldf"
