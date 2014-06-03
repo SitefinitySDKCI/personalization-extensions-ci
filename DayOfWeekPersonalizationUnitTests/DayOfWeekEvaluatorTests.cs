e3b9147126b88e7ce3abce1d320fe3273151458d
nader-dab <nader.dabour@abv.bg>
2014-06-03 14:52:24 +0300

    Added unit test for DayOfWeekEvaluator

---
+++ DayOfWeekPersonalizationUnitTests/DayOfWeekEvaluatorTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayOfWeekPersonalization;
+using Telerik.Sitefinity.Personalization;
+using Telerik.Sitefinity.Personalization.Impl;
+
+namespace DayOfWeekPersonalizationUnitTests
+{
+    [TestClass]
+    public class DayOfWeekEvaluatorTests
+    {
+        [TestMethod]
+        public void TestDayOfWeekEvaluatorReturnsTrueWhenDateTimeIsMondayAndSettingsIsMonday()
+        {
+            // arrange
+            DateTime mondayDate = new DateTime(2014, 6, 2); 
+            string mondayString = ((int)mondayDate.DayOfWeek).ToString();
+            DayOfWeekEvaluator dayOfWeekEvaluator = new DayOfWeekEvaluator();
+
+            // act
+            dayOfWeekEvaluator.CurrentDateTime = mondayDate;
+            bool expectedResult = dayOfWeekEvaluator.IsMatch(mondayString, new PersonalizationTestContext());
+
+            // assert
+            Assert.IsTrue(expectedResult);
+        }
+
+        [TestMethod]
+        public void TestDayOfWeekEvaluatorReturnsFalseWhenDateTimeIsMondayAndSettingsIsTuesday()
+        {
+            // arrange
+            DateTime mondayDate = new DateTime(2014, 6, 2);
+            DateTime tuesdayDate = mondayDate.AddDays(1);
+            string tuesdayString = ((int)tuesdayDate.DayOfWeek).ToString();
+            DayOfWeekEvaluator dayOfWeekEvaluator = new DayOfWeekEvaluator();
+
+            // act
+            dayOfWeekEvaluator.CurrentDateTime = mondayDate;
+            bool expectedResult = dayOfWeekEvaluator.IsMatch(tuesdayString, new PersonalizationTestContext());
+
+            // assert
+            Assert.IsFalse(expectedResult);
+        }
+    }
+}
