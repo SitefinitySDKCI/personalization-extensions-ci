73249db31066d9497dd8952b4e19cd440c2ef8ee
nader-dab <nader.dabour@abv.bg>
2014-06-03 10:03:34 +0300

    Added that fails

--- DayOfWeekPersonalizationTests/PagesModuleTests.cs
+++ DayOfWeekPersonalizationTests/PagesModuleTests.cs
@@ -28,6 +28,15 @@ namespace DayOfWeekPersonalizationTests
             Assert.IsTrue(true);
         }
 
+        [Test]
+        [Category(TestCategories.Sdk)]
+        [Description("A test that fails.")]
+        [Author("SDK")]
+        public void TestThatAlwaysFails()
+        {
+            Assert.IsTrue(false);
+        }
+
         [Test][Category(TestCategories.Sdk)]
         [Description("Verifies that a page can be created using the Native API.")]
         [Author("SDK")]
